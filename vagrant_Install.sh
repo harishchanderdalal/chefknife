4e05331d01f8aa63e4834faeed4116435e411c1b
Harish Chander Dalal <harish.chander@hotmail.com>
2017-08-27 12:31:13 +0530

    Update vagrant_Install.sh

--- vagrant_Install.sh
+++ vagrant_Install.sh
@@ -10,4 +10,3 @@ sudo dpkg -i vagrant_1.9.7_x86_64.deb
 sudo apt-get install -y build-essential patch
 sudo apt-get install -y ruby-dev zlib1g-dev liblzma-dev
 vagrant plugin install vagrant-aws
-vagrant box add dummy https://github.com/mitchellh/vagrant-aws/raw/master/dummy.box
