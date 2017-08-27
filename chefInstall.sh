d090c38a9aaedbd5c6e4ac09f9e3a46ebc1795b8
Harish Chander Dalal <harish.chander@hotmail.com>
2017-08-27 15:34:27 +0530

    Update chefInstall.sh

--- chefInstall.sh
+++ chefInstall.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
+echo BEGIN
 sudo apt-get update
+sudo apt-get install -y wget
 wget https://packages.chef.io/files/stable/chef-server/12.16.2/ubuntu/16.04/chef-server-core_12.16.2-1_amd64.deb
 sudo dpkg -i chef-server*
+
+sudo chef-server-ctl reconfigure
 sudo chef-server-ctl status
 sudo chef-server-ctl test
+
+sudo chef-server-ctl user-create harishchanderdalal harish chander harish.chander@hotmail.com admin@123$
+
 sudo chef-server-ctl install chef-manage 
 sudo chef-server-ctl reconfigure
 sudo chef-manage-ctl reconfigure --accept-license
-sudo chef-server-ctl user-create harishchanderdalal harish chander harish.chander@hotmail.com admin@123$
-sudo chef-manage-ctl restart
-sudo chef-server-ctl restart
 
 sudo chef-server-ctl install opscode-push-jobs-server
 sudo chef-server-ctl reconfigure
@@ -18,3 +22,7 @@ sudo opscode-push-jobs-server-ctl reconfigure
 sudo chef-server-ctl install opscode-reporting
 sudo chef-server-ctl-reconfigure
 sudo opscode-reporting-ctl reconfigure --accept-license
+
+sudo chef-manage-ctl restart
+sudo chef-server-ctl restart
+echo END
