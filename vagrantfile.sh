c3abfe2627e2f9e4cf21c0987e767b6b1fa2a73b
Harish Chander Dalal <harish.chander@hotmail.com>
2017-08-27 14:42:29 +0530

    Update vagrantfile.sh

--- vagrantfile.sh
+++ vagrantfile.sh
@@ -3,16 +3,16 @@ cat <<EOF
 Vagrant.configure('2') do |config|
     config.vm.box = 'dummy'
     config.vm.provider 'aws' do |aws, override|
-    aws.access_key_id = "$key"
-    aws.secret_access_key = "$access"
+    aws.access_key_id = "$accesskey"
+    aws.secret_access_key = "$secretkey"
     aws.keypair_name = '$privatekeyname'
     aws.instance_type = 't2.medium'
-    aws.region = 'region'
+    aws.region = '$region'
     aws.ami = 'ami-099fe766'
     aws.security_groups = '$sgroup'
     aws.tags = {
-        'Name' => 'tag',
-        'Owner' => 'owner'
+        'Name' => '$tag',
+        'Owner' => '$owner'
     }
     override.ssh.username = 'ubuntu'
     override.ssh.private_key_path = '/var/lib/jenkins/workspace/$JOB_NAME/vagrant/keypair.pem'
