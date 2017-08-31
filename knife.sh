7fa978c6e26e0d1a8f7248907daa0879046381cb
Harish Chander Dalal <harish.chander@hotmail.com>
2017-09-01 00:53:36 +0530

    Create knife.sh

---
+++ knife.sh
@@ -0,0 +1,10 @@
+#! /bin/sh
+cat <<EOF
+current_dir = File.dirname(__FILE__)
+log_level                :info
+log_location             STDOUT
+node_name                "admin"
+client_key               "#{current_dir}/admin.pem"
+chef_server_url          "https://$chefserver/organizations/agilityroots"
+cookbook_path            ["#{current_dir}/../cookbooks"]
+EOF
