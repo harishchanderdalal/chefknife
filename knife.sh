ac7b5e040b20f7430b1d509b069e14e7af91b49c
Harish Chander Dalal <harish.chander@hotmail.com>
2017-09-01 01:04:37 +0530

    Update knife.sh

--- knife.sh
+++ knife.sh
@@ -5,6 +5,6 @@ log_level                :info
 log_location             STDOUT
 node_name                "admin"
 client_key               "#{current_dir}/admin.pem"
-chef_server_url          "https://$chefserver/organizations/agilityroots"
+chef_server_url          "https://$chefurl/organizations/agilityroots"
 cookbook_path            ["#{current_dir}/../cookbooks"]
 EOF
